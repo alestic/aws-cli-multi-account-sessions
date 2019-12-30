3b88200d4d0b2dbcb1a5026c23c9c92d62696b6b
Eric Hammond <ehammond@thinksome.com>
2019-12-29 18:29:13 -0800

    First draft of code and documentation.

--- README.md
+++ README.md
@@ -1,2 +1,85 @@
-# aws-cli-cross-account-sessions
-bash functions to help run aws-cli commands across roles in multiple accounts with MFA
+# aws-cli-multi-account-sessions
+
+bash functions to help run aws-cli commands across roles in multiple
+accounts with MFA
+
+## Blog Post
+
+For more background information and explaination for how to use this,
+please read this blog post:
+
+> <https://alestic.com/2019/12/aws-cli-across-organization-accounts/>
+
+## Setup
+
+You didn't read that, did you?
+
+Ok, here are the quick notes I use to set this up and use it in my
+accounts.
+
+Clone this repo wherever you like:
+
+    mkdir -p src
+    cd src
+    git clone git@github.com:alestic/aws-cli-multi-account-sessions.git
+
+Add something like this to `$HOME/.bashrc` using the values for
+`source_profile` and `mfa_serial` from your aws-cli config file.
+
+    # https://github.com/alestic/aws-cli-multi-account-sessions
+    test -x src/aws-cli-multi-account-sessions/functions.sh &&
+     source src/aws-cli-multi-account-sessions/functions.sh
+    export AWS_SESSION_SOURCE_PROFILE=your_aws_cli_source_profile
+    export AWS_SESSION_MFA_SERIAL=arn:aws:iam::YOUR_ACCOUNT:mfa/YOUR_USER
+
+    source $HOME/.bashrc
+
+## Usage
+
+Specify the role you can assume in all accounts:
+
+    role="admin"
+
+Get a list of all accounts in the AWS Organization:
+
+    accounts=$(aws organizations list-accounts \
+      --output text \
+      --query 'Accounts[].[JoinedTimestamp,Status,Id,Email,Name]' |
+      grep ACTIVE |
+      sort |
+      cut -f3) # just the ids
+    echo "$accounts"
+
+Run once to create temporary session credentials with MFA:
+
+    aws-session-init
+
+Iterate through AWS accounts, running AWS CLI commands in each
+account/role:
+
+    for account in $accounts; do
+      aws-session-set $account $role || continue
+
+      this_account=$(aws-session-run \
+                       aws sts get-caller-identity \
+                         --output text \
+                         --query 'Account')
+      echo "Account: $account ($this_account)"
+
+      aws-session-run aws s3 ls
+    done
+
+Clear out bash variables holding temporary credentials:
+
+    aws-session-cleanup
+
+Of course, this might not work for you if you don't have things set up
+quite the same way as me. Perhaps you should go back and read the blog
+post above?
+
+## Support (not)
+
+This is not intended to be fully supported open source software. You
+may copy and use it as you see fit, but I am less likely than usual to
+add feature enhancements on request or be able to debug your AWS
+account setup for this.
---
+++ functions.sh
@@ -0,0 +1,77 @@
+#
+# aws-cli-multi-account-sessions/functions.sh
+#
+# bash functions to help run aws-cli commands across roles in multiple
+# accounts with MFA
+#
+# See also: https://github.com/alestic/aws-cli-multi-account-sessions
+# See also: https://alestic.com/2019/12/aws-cli-across-organization-accounts/
+#
+
+#
+# Get temporary session credentials with MFA. These are used to
+# generate temporary assume-role credentials for different
+# accounts/roles without having to re-enter MFA tokens for each.
+#
+aws-session-init() {
+  # Sets: source_access_key_id source_secret_access_key source_session_token
+  local source_profile=${1:-${AWS_SESSION_SOURCE_PROFILE:?source profile must be specified}}
+  local mfa_serial=${2:-$AWS_SESSION_MFA_SERIAL}
+  local token_code=
+  local mfa_options=
+  if [ -n "$mfa_serial" ]; then
+    read -s -p "Enter MFA code for $mfa_serial: " token_code
+    echo
+    mfa_options="--serial-number $mfa_serial --token-code $token_code"
+  fi
+  read -r source_access_key_id \
+          source_secret_access_key \
+          source_session_token \
+    <<<$(aws sts get-session-token \
+           --profile $source_profile \
+           $mfa_options \
+           --output text \
+           --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]')
+}
+
+#
+# Get temporary assume-role credentials for a specific account/role,
+# using the temporary session credentials from aws-session-init above.
+#
+aws-session-set() {
+  # Sets: aws_access_key_id aws_secret_access_key aws_session_token
+  local account=$1
+  local role=$2
+  local name=${3:-aws-session-access}
+  read -r aws_access_key_id \
+          aws_secret_access_key \
+          aws_session_token \
+    <<<$(AWS_ACCESS_KEY_ID=$source_access_key_id \
+         AWS_SECRET_ACCESS_KEY=$source_secret_access_key \
+         AWS_SESSION_TOKEN=$source_session_token \
+         aws sts assume-role \
+           --role-arn arn:aws:iam::$account:role/$role \
+           --role-session-name "$name" \
+           --output text \
+           --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]')
+  test -n "$aws_access_key_id" && return 0 || return 1
+}
+
+#
+# Run an AWS command using the temporary assume-role credentials from
+# aws-session-set above.
+#
+aws-session-run() {
+  AWS_ACCESS_KEY_ID=$aws_access_key_id \
+  AWS_SECRET_ACCESS_KEY=$aws_secret_access_key \
+  AWS_SESSION_TOKEN=$aws_session_token \
+    "$@"
+}
+
+#
+# Clear out temporary credentials for security.
+#
+aws-session-cleanup() {
+  unset source_access_key_id source_secret_access_key source_session_token
+  unset    aws_access_key_id    aws_secret_access_key    aws_session_token
+}
